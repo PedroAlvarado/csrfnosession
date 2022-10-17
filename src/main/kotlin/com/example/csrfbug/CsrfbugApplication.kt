0169bfc024738e3974d3ce6efdf097132ac3684e
Pedro Alvarado <pedro.alvarado@kabzeel.com>
2022-10-17 14:09:34 -0400

    reproducer

---
+++ src/main/kotlin/com/example/csrfbug/CsrfbugApplication.kt
@@ -0,0 +1,53 @@
+package com.example.csrfbug
+
+import kotlinx.coroutines.reactive.awaitSingle
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity.http
+import org.springframework.security.config.web.server.invoke
+import org.springframework.security.web.reactive.result.view.CsrfRequestDataValueProcessor
+import org.springframework.security.web.server.SecurityWebFilterChain
+import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository
+import org.springframework.security.web.server.csrf.CsrfToken
+import org.springframework.security.web.server.util.matcher.PathPatternParserServerWebExchangeMatcher
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.buildAndAwait
+import org.springframework.web.reactive.function.server.coRouter
+import reactor.core.publisher.Mono
+
+@SpringBootApplication
+@EnableConfigurationProperties
+class CsrfbugApplication
+
+fun main(args: Array<String>) {
+    runApplication<CsrfbugApplication>(*args)
+}
+
+@Configuration
+class Configs {
+
+    @Bean
+    fun router() = coRouter {
+        GET("/web") { ServerResponse.ok().buildAndAwait() }
+    }
+
+    @Bean
+    fun springSecurityFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
+        return http {
+            httpBasic {  }
+            formLogin {  }
+            authorizeExchange {
+                authorize(anyExchange, authenticated)
+            }
+            csrf {
+                csrfTokenRepository = CookieServerCsrfTokenRepository.withHttpOnlyFalse()
+            }
+        }
+    }
+
+}
---
+++ src/test/kotlin/com/example/csrfbug/CsrfbugApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.csrfbug
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CsrfbugApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
